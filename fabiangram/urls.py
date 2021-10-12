fb1359646c96134c16be55993dfd0ae11802dd84
Fabián Ardila <fabianra97@gmail.com>
2021-10-12 09:07:38 -0500

    :sparkles: create a hello world

--- fabiangram/urls.py
+++ fabiangram/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+def hello_world(request):
+    return HttpResponse('Hellos world')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello-world', hello_world)
 ]
