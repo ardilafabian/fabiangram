f019cf2aa6f699ca3522b652aa46fdaf8ffb9eaf
Fabián Ardila <fabianra97@gmail.com>
2021-10-13 08:59:57 -0500

    :sparkles: Send paramenters in URL converter

--- fabiangram/views.py
+++ fabiangram/views.py
@@ -7,8 +7,11 @@ def hello_world(request):
     now = datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
     return HttpResponse('Oh, Hi! vurrente server time is {now}'.format(now=str(now)))
 
-def hi(request):
+def sort_integers(request):
     numbers = [int(x) for x in request.GET['numbers'].split(',')]
     numbers.sort()
     response = {'nums':numbers}
     return JsonResponse(response)
+
+def say_hi(request, name, age):
+    return HttpResponse('hi '+ str(name)+str(age))
