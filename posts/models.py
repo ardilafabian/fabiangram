5ab2709af27de68aae68f05700f6c7f97dd52543
Fabián Ardila <fabianra97@gmail.com>
2021-10-14 09:28:27 -0500

    :construction: create models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+""""Post models"""
 
-# Create your models here.
+# Djagno
+from django.db import models
+from django.db.models.fields import DateField
+
+class User(models.Model):
+    """User model."""
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    # Tiene mas caracteres que un CharField
+    bio = models.TextField(blank=True)
+
+    birthdate = models.DateField(blank=True, null=True)
+
+    # Este alamcena también la hora
+    created = models.DateTimeField(auto_now_add=True) # Guarda la fecha de creación
+    modified = models.DateTimeField(auto_now=True) # Guarda la fecha de ultima edición
+
