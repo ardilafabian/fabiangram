140fa7f0d550c9988df60cda8ebfc9498938a343
Fabián Ardila <fabianra97@gmail.com>
2021-10-14 20:17:17 -0500

    :sparkles: Update models

--- posts/models.py
+++ posts/models.py
@@ -12,12 +12,19 @@ class User(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    is_admin = models.BooleanField(default=False)
+
     # Tiene mas caracteres que un CharField
     bio = models.TextField(blank=True)
 
     birthdate = models.DateField(blank=True, null=True)
 
+    country = models.CharField(max_length=50, null=True)
+
     # Este alamcena también la hora
     created = models.DateTimeField(auto_now_add=True) # Guarda la fecha de creación
     modified = models.DateTimeField(auto_now=True) # Guarda la fecha de ultima edición
 
+    def __str__(self):
+        """Return self.email"""
+        return self.email
