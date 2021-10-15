2bf593230ae3334328b6fe4224bc4174d2b7ac73
Fabián Ardila <fabianra97@gmail.com>
2021-10-15 09:27:07 -0500

    :sparkles: Create new Users app withproxu model

---
+++ users/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.fields.json import DataContains
+
+class Profile(models.Model):
+    """Profile model.
+    
+    Proxy model that extendes the base data with other 
+    information
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    website = models.URLField(max_length=200, blank=True)
+    biografy = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+
+    """
+    Resolve pillow importing 
+    
+    picture = models.ImageField(
+        upload_to='users/pictures', 
+        blank=True, 
+        null=True
+    )"""
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return username"""
+        return self.user.username
